d0d9932180658d9cd9f42d2befd77669a78b6a71
Calvin C <curtindolph.calvin.10@gmail.com>
2026-03-19 22:35:53 -0500

    calvincTools.__init__ import package vars from __version__

--- calvincTools/__init__.py
+++ calvincTools/__init__.py
@@ -1,4 +1,9 @@
-from .__version__ import *
+from __version__ import (
+    _pkgname,
+    _ver_date,
+    _base_ver, __version__, sysver, sysver_key,
+    __author__, __email__, 
+    )
 
 # Import main modules here as needed
 # from .module import function
