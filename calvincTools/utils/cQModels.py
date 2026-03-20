2d8c920e3d3980fde75197135bd446c274dd7c50
Calvin C <curtindolph.calvin.10@gmail.com>
2026-03-19 22:26:44 -0500

    bug fixes

--- calvincTools/utils/cQModels.py
+++ calvincTools/utils/cQModels.py
@@ -135,10 +135,12 @@ class SQLAlchemyTableModel(QAbstractTableModel):
         """Reload data from the database"""
         with self.session_factory() as session:
             stmt = select(self.model_class)
-            if filter:
+            if filter is not None:
+                if not isinstance(filter, (list, tuple)):
+                    filter = [filter]
                 stmt = stmt.where(*filter)
             if orderby is not None:
-                stmt = stmt.order_by(*orderby)
+                stmt = stmt.order_by(orderby)
             rows = session.execute(stmt).scalars().all()
             for row in rows:
                 session.expunge(row)  # detach from session
