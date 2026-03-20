499b9c2914eba45f9c1c531b77b36e8136dfe704
Calvin C <curtindolph.calvin.10@gmail.com>
2026-03-20 09:51:27 -0500

    import all __version__ vars in calvincTools.__init__ (mostly so tests pass)

--- calvincTools/__init__.py
+++ calvincTools/__init__.py
@@ -1,7 +1,8 @@
 from .__version__ import (
     _pkgname,
     _ver_date,
-    _base_ver, __version__, sysver, sysver_key,
+    _base_ver_major, _base_ver_minor, _base_ver_patch, _base_ver, 
+    __version__, sysver, sysver_key,
     __author__, __email__, 
     )
 
