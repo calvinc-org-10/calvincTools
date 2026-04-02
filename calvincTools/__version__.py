4c142ed53ae9b9fb2283fb5057a8537df9ac61d1
Calvin C <curtindolph.calvin.10@gmail.com>
2026-04-02 12:41:51 -0500

    WIP - final tweaks on EditMenu. almost ready to refactor _newcode.py and merge into main

--- calvincTools/__version__.py
+++ calvincTools/__version__.py
@@ -6,7 +6,7 @@ _pkgname='Calvin C Tools'
 _base_ver_major=2
 _base_ver_minor=0
 _base_ver_patch='0'
-_ver_date='2026-03-??'
+_ver_date='2026-04-02'
 _base_ver = f'{_base_ver_major}.{_base_ver_minor}.{_base_ver_patch}'
 __version__ = _base_ver
 sysver = {
@@ -21,7 +21,7 @@ __author__ = "Calvin C"
 __email__ = "calvinc404@gmail.com"
 
 # Change Log:
-# 2.0.0 - 2026-03-?? - rewrite of cQdbRecordForm classes, notably introducing and using
+# 2.0.0 - 2026-04-02 - rewrite of cQdbRecordForm classes, notably introducing and using
 #               cFormFieldDefs and cFormLayouts to replace dicts. Also restructured Form Class
 #               methods extensively
 # 1.8.3 - 2026-03-19 - bug fixes and code cleanup. Hopefully, conditions are passed properly now in cQdbSimpleRecordSubFormWidgets and cSimpleMultiRecordSubFmWrapperForm. Other minor bug fixes and code cleanup.
