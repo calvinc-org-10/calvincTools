c255c24b7b06be8e4d024f39cf3aacb47b0970ae
Calvin C <curtindolph.calvin.10@gmail.com>
2026-04-13 08:35:18 -0500

    WIP - setting up login/User structures

--- calvincTools/__init__.py
+++ calvincTools/__init__.py
@@ -11,42 +11,47 @@ from .__version__ import (
 
 from .apphooks import cTools_apphooks
 
-def calvincTools_init(usr_auth: bool,
-    app_sessionmaker=None, 
-    FormNameToURL_Map={},
-    ExternalWebPageURL_Map={},
-    appver='',
-    **kwargs
-    ):
-    """
-    Initialize calvincTools
-    Pass in structures that calvincTools will need which are cntrolled by calling app
+class calvincTools_init:
+    _cMenu = None
 
-    Returns:
-        None
-    """
-    ## dirty little secret: this started life as calvincTools_apphooks, but I wanted to add some more app-specific initialization here.
-    ## so I renamed it to calvincTools_init, but calvincTools_apphooks still exists and still does the heavy lifting. 
-    ## However, I want to keep the apphooks separate from the main initialization, so that they can be used independently if needed.
-    ## apps should call calvincTools_init, not calvincTools_apphooks, but calvincTools_init will call calvincTools_apphooks.initialize to set up the hooks.
 
-    # Initialize app hooks - this is the main point of this function, to ensure that the hooks are set up before any
-    cTools_apphooks.initialize(
-        app_sessionmaker=app_sessionmaker, 
-        FormNameToURL_Map=FormNameToURL_Map,
-        ExternalWebPageURL_Map=ExternalWebPageURL_Map,
-        appver=appver,
+    def __init__(self, 
+        usr_auth: bool,
+        app_sessionmaker=None, 
+        FormNameToURL_Map={},
+        ExternalWebPageURL_Map={},
+        appver='',
         **kwargs
-    )
-    
-    # login, if usr_auth is True
-    if usr_auth:
-        ...
-        # init_login_manager(app)
-            
-        # Create default Calvin user if not exists
-        # R E M O V E   I N   P R O D U C T I O N   ! ! !
-        # create_calvin(app)
+        ):
+        """
+        Initialize calvincTools
+        Pass in structures that calvincTools will need which are cntrolled by calling app
+
+        Returns:
+            None
+        """
+        ## dirty little secret: this started life as calvincTools_apphooks, but I wanted to add some more app-specific initialization here.
+        ## so I renamed it to calvincTools_init, but calvincTools_apphooks still exists and still does the heavy lifting. 
+        ## However, I want to keep the apphooks separate from the main initialization, so that they can be used independently if needed.
+        ## apps should call calvincTools_init, not calvincTools_apphooks, but calvincTools_init will call calvincTools_apphooks.initialize to set up the hooks.
+
+        # Initialize app hooks - this is the main point of this function, to ensure that the hooks are set up before any
+        cTools_apphooks.initialize(
+            app_sessionmaker=app_sessionmaker, 
+            FormNameToURL_Map=FormNameToURL_Map,
+            ExternalWebPageURL_Map=ExternalWebPageURL_Map,
+            appver=appver,
+            **kwargs
+        )
+        
+        # login, if usr_auth is True
+        if usr_auth:
+            ...
+            # init_login_manager(app)
+                
+            # Create default Calvin user if not exists
+            # R E M O V E   I N   P R O D U C T I O N   ! ! !
+            # create_calvin(app)
     
     # implement later
     # cTools_tables = (None, None, None, None, None)   # will be set by init_cDatabase
--- calvincTools/usr_auth/storage.py
+++ calvincTools/usr_auth/storage.py
@@ -2,13 +2,20 @@
 miniauth.storage
 ~~~~~~~~~~~~~~~~
 Storage impelentations
+
+Adapted from MiniAuth: https://github.com/farzadghanei/miniauth/tree/master
 """
+
 from __future__ import absolute_import
+from .typing import Any, AnyStr, Iterable, List, Mapping, Text, Tuple
+from typing import Type
+
 import sqlite3
 from logging import getLogger
 from abc import ABCMeta, abstractmethod
 from contextlib import closing
-from .typing import Any, AnyStr, Iterable, List, Mapping, Text, Tuple
+
+from calvincTools.database import (Repository, get_cMenu_sessionmaker)
 
 
 logger = getLogger(__name__)
@@ -26,14 +33,12 @@ class AbstractStorage(object):
     """
     __metaclass__ = ABCMeta
 
-    def __init__(self, db_path):
-        # type: (Text) -> None
-        self._db_path = db_path  # type: Text
+    def __init__(self, user_model):
+        self._user_model = user_model
 
     @property
-    def db_path(self):
-        # type: () -> Text
-        return self._db_path
+    def user_model(self):
+        return self._user_model
 
     def choose_default_hash_func(self, hash_funcs):
         # type: (Iterable[str]) -> str
@@ -57,8 +62,7 @@ class AbstractStorage(object):
         pass
 
     @abstractmethod
-    def get_record(self, user):
-        # type: (Text) -> Mapping[Text, Any]
+    def get_record(self, user:int) -> Any:
         """Return the record regarding the user.
         If user does not exist, return empty values for the record.
         Return dict keys should be:
@@ -68,13 +72,16 @@ class AbstractStorage(object):
             salt: (str) salt used for hashing
             disabled: (bool) if user is disabled [optional]
         """
-        return {
-            'username': user,
-            'password': '',
-            'hash_func': '',
-            'salt': '',
-            'disabled': False,
-        }
+        # return {
+        #     'username': user,
+        #     'password': '',
+        #     'hash_func': '',
+        #     'salt': '',
+        #     'disabled': False,
+        # }
+        ssnmaker = get_cMenu_sessionmaker()
+        modl = self.user_model()
+        return Repository(ssnmaker, modl).get_by_id(user)
 
     @abstractmethod
     def update_record(self, user, password_hash, hash_func, salt):
@@ -120,142 +127,142 @@ SQLITE_SCHEMA = {
 }  # type: Mapping[int, Mapping[str, Any]]
 
 
-class SqliteStorage(AbstractStorage):
-    """Storage backend using SQLite database files"""
+# class SqliteStorage(AbstractStorage):
+#     """Storage backend using SQLite database files"""
 
-    def _query_db(self, query, params=()):
-        # type: (AnyStr, Iterable[AnyStr]) -> Tuple[List[Any], int, int]
-        """Run the query with optional parameters, return
-        all result rows, row count and last row id as a tuple
-        """
-        db_con = sqlite3.connect(self.db_path)
-        cursor = db_con.cursor()
-        with closing(db_con):
-            with closing(cursor):
-                cursor.execute(str(query), params)
-                db_con.commit()
-                query_results = cursor.fetchall()
-                row_count = cursor.rowcount
-                last_row_id = cursor.lastrowid
-        return query_results, row_count, last_row_id
+#     def _query_db(self, query, params=()):
+#         # type: (AnyStr, Iterable[AnyStr]) -> Tuple[List[Any], int, int]
+#         """Run the query with optional parameters, return
+#         all result rows, row count and last row id as a tuple
+#         """
+#         db_con = sqlite3.connect(self.user_model)
+#         cursor = db_con.cursor()
+#         with closing(db_con):
+#             with closing(cursor):
+#                 cursor.execute(str(query), params)
+#                 db_con.commit()
+#                 query_results = cursor.fetchall()
+#                 row_count = cursor.rowcount
+#                 last_row_id = cursor.lastrowid
+#         return query_results, row_count, last_row_id
 
-    def _multi_query(self, queries):
-        # type: (Iterable[AnyStr]) -> None
-        """Run the queries against the database. Useful for DDL queries."""
-        db_con = sqlite3.connect(self.db_path)
-        cursor = db_con.cursor()
-        with closing(db_con):
-            with closing(cursor):
-                for query in queries:
-                    cursor.execute(str(query))
-                db_con.commit()
+#     def _multi_query(self, queries):
+#         # type: (Iterable[AnyStr]) -> None
+#         """Run the queries against the database. Useful for DDL queries."""
+#         db_con = sqlite3.connect(self.user_model)
+#         cursor = db_con.cursor()
+#         with closing(db_con):
+#             with closing(cursor):
+#                 for query in queries:
+#                     cursor.execute(str(query))
+#                 db_con.commit()
 
-    def _get_schema_version(self):
-        # type: () -> int
-        """Return current DB schema version, return 0 if version can't be detected"""
-        try:
-            rows, _, _ = self._query_db('SELECT MAX(version) FROM meta')
-            if rows:
-                return rows[0][0]
-        except sqlite3.OperationalError as exp:
-            pass
-        return 0
+#     def _get_schema_version(self):
+#         # type: () -> int
+#         """Return current DB schema version, return 0 if version can't be detected"""
+#         try:
+#             rows, _, _ = self._query_db('SELECT MAX(version) FROM meta')
+#             if rows:
+#                 return rows[0][0]
+#         except sqlite3.OperationalError as exp:
+#             pass
+#         return 0
 
-    def _apply_schema_version(self, version):
-        # type: (int) -> None
-        self._multi_query(SQLITE_SCHEMA[version]['tables'].values())
-        self._multi_query(['INSERT OR REPLACE INTO meta (version) VALUES ({})'.format(version)])
-        logger.info('DB schema updated to version {} on "{}"'.format(self.db_path, version))
+#     def _apply_schema_version(self, version):
+#         # type: (int) -> None
+#         self._multi_query(SQLITE_SCHEMA[version]['tables'].values())
+#         self._multi_query(['INSERT OR REPLACE INTO meta (version) VALUES ({})'.format(version)])
+#         logger.info('DB schema updated to version {} on "{}"'.format(self.user_model, version))
 
-    def _ensure_schema(self):
-        # type: () -> None
-        cur_version = self._get_schema_version()
-        if not cur_version:
-            # no schema yet (new database), create latest schema
-            logger.info('No DB detected on "{}". Creating latest DB schema ...'.format(self.db_path))
-            version = max(SQLITE_SCHEMA.keys())
-            self._apply_schema_version(version)
-        # @TODO: implement migrating from previous schema
+#     def _ensure_schema(self):
+#         # type: () -> None
+#         cur_version = self._get_schema_version()
+#         if not cur_version:
+#             # no schema yet (new database), create latest schema
+#             logger.info('No DB detected on "{}". Creating latest DB schema ...'.format(self.user_model))
+#             version = max(SQLITE_SCHEMA.keys())
+#             self._apply_schema_version(version)
+#         # @TODO: implement migrating from previous schema
 
-    def _pre_write(self):
-        # type: () -> None
-        self._ensure_schema()
+#     def _pre_write(self):
+#         # type: () -> None
+#         self._ensure_schema()
 
-    def _pre_read(self):
-        # type: () -> None
-        self._ensure_schema()
+#     def _pre_read(self):
+#         # type: () -> None
+#         self._ensure_schema()
 
-    def create_record(self, user, password_hash, hash_func, salt):
-        # type: (Text, Text, str, str) -> None
-        self._pre_write()
-        self._query_db(
-            'INSERT INTO user (username, password, hash, salt) VALUES (?, ?, ?, ?)',
-            (user, password_hash, hash_func, salt)
-        )
+#     def create_record(self, user, password_hash, hash_func, salt):
+#         # type: (Text, Text, str, str) -> None
+#         self._pre_write()
+#         self._query_db(
+#             'INSERT INTO user (username, password, hash, salt) VALUES (?, ?, ?, ?)',
+#             (user, password_hash, hash_func, salt)
+#         )
 
-    def record_exists(self, user):
-        # type: (Text) -> bool
-        self._pre_read()
-        rows, _, _ = self._query_db(
-            'SELECT username FROM user WHERE username = ?',
-            (user,)
-        )
-        return len(rows) > 0
+#     def record_exists(self, user):
+#         # type: (Text) -> bool
+#         self._pre_read()
+#         rows, _, _ = self._query_db(
+#             'SELECT username FROM user WHERE username = ?',
+#             (user,)
+#         )
+#         return len(rows) > 0
 
-    def get_record(self, user):
-        # type: (Text) -> Mapping
-        self._pre_read()
-        rows, _, _ = self._query_db(
-            'SELECT password, hash, salt, disabled FROM user WHERE username = ?',
-            (user,)
-        )
-        record = {
-            'username': user,
-            'password': '',
-            'hash_func': '',
-            'salt': '',
-            'disabled': False,
-        }
-        if rows:
-            row = rows[0]
-            record['password'] = row[0]
-            record['hash_func'] = row[1]
-            record['salt'] = row[2]
-            record['disabled'] = bool(row[3])
-        return record
+#     def get_record(self, user):
+#         # type: (Text) -> Mapping
+#         self._pre_read()
+#         rows, _, _ = self._query_db(
+#             'SELECT password, hash, salt, disabled FROM user WHERE username = ?',
+#             (user,)
+#         )
+#         record = {
+#             'username': user,
+#             'password': '',
+#             'hash_func': '',
+#             'salt': '',
+#             'disabled': False,
+#         }
+#         if rows:
+#             row = rows[0]
+#             record['password'] = row[0]
+#             record['hash_func'] = row[1]
+#             record['salt'] = row[2]
+#             record['disabled'] = bool(row[3])
+#         return record
 
-    def update_record(self, user, password_hash, hash_func, salt):
-        # type: (Text, Text, str, str) -> bool
-        self._pre_write()
-        _, row_count, _ = self._query_db(
-            'UPDATE user SET password = ?, hash = ?, salt = ? WHERE username = ?',
-            (password_hash, hash_func, salt, user)
-        )
-        return row_count == 1
+#     def update_record(self, user, password_hash, hash_func, salt):
+#         # type: (Text, Text, str, str) -> bool
+#         self._pre_write()
+#         _, row_count, _ = self._query_db(
+#             'UPDATE user SET password = ?, hash = ?, salt = ? WHERE username = ?',
+#             (password_hash, hash_func, salt, user)
+#         )
+#         return row_count == 1
 
-    def enable_record(self, user):
-        # type: (Text) -> bool
-        self._pre_write()
-        _, row_count, _ = self._query_db(
-            'UPDATE user set disabled = 0 WHERE username = ?',
-            (user,)
-        )
-        return row_count == 1
+#     def enable_record(self, user):
+#         # type: (Text) -> bool
+#         self._pre_write()
+#         _, row_count, _ = self._query_db(
+#             'UPDATE user set disabled = 0 WHERE username = ?',
+#             (user,)
+#         )
+#         return row_count == 1
 
-    def disable_record(self, user):
-        # type: (Text) -> bool
-        self._pre_write()
-        _, row_count, _ = self._query_db(
-            'UPDATE user set disabled = 1 WHERE username = ?',
-            (user,)
-        )
-        return row_count == 1
+#     def disable_record(self, user):
+#         # type: (Text) -> bool
+#         self._pre_write()
+#         _, row_count, _ = self._query_db(
+#             'UPDATE user set disabled = 1 WHERE username = ?',
+#             (user,)
+#         )
+#         return row_count == 1
 
-    def delete_record(self, user):
-        # type: (Text) -> bool
-        self._pre_write()
-        _, row_count, _ = self._query_db(
-            'DELETE FROM user WHERE username = ?',
-            (user,)
-        )
-        return row_count == 1
+#     def delete_record(self, user):
+#         # type: (Text) -> bool
+#         self._pre_write()
+#         _, row_count, _ = self._query_db(
+#             'DELETE FROM user WHERE username = ?',
+#             (user,)
+#         )
+#         return row_count == 1
