4e9b84ca14e860dd57f902b7e493547856bfda8d
Calvin C <curtindolph.calvin.10@gmail.com>
2026-05-11 13:44:18 -0500

    calvincTools-cpp moved to its own repository

--- calvincTools-cpp/src/database/models.cpp
+++
@@ -1,7 +0,0 @@
-#include "calvinc_tools/database/models.hpp"
-
-namespace calvinc::tools::database {
-
-// Models are header-only structures, no implementation needed
-
-}  // namespace calvinc::tools::database
